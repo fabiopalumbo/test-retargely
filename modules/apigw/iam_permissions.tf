6d51a889cf14c727c6e4dbad349cdd6e7e0ae8a9
Fabio Palumbo <fabioppalumbo@gmail.com>
2022-03-21 21:50:37 -0300

    TEST-017 | Fabio Palumbo | Invoke

--- modules/apigw/iam_permissions.tf
+++ modules/apigw/iam_permissions.tf
@@ -1,16 +1,37 @@
-{
-  "Version": "2012-10-17",
-  "Id": "default",
-  "Statement": [
-    {
-      "Sid": "apig-functiongetEndpointPermission-BWDBXMPLXE2F",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "apigateway.amazonaws.com"
-      },
-      "Action": "lambda:InvokeFunction",
-      "Resource": "${var.lambda_arn}",
-
+#Define a policy which will allow APIGW to Assume an IAM Role
+data "aws_iam_policy_document" "apigw_assume_role" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["apigateway.amazonaws.com"]
     }
-  ]
+  }
+}
+
+# Define a policy which will allow APIGW to access your s3
+data "aws_iam_policy_document" "api_gw_lambda_invoke_policy" {
+  statement {
+      effect = "Allow"
+      actions = [
+        "lambda:InvokeFunction"
+      ]
+      resources = [
+        "${var.lambda_arn}",
+      ]
+  }
+}
+
+#creates a new iam role
+resource "aws_iam_role" "api_gw_role" {
+  name               = "${var.identifier}-api_gw_role-${random_string.this.id}"
+  assume_role_policy = data.aws_iam_policy_document.apigw_assume_role.json
+}
+
+#attach s3 access policy
+resource "aws_iam_role_policy" "api_gw_access_s3_policy" {
+  name   = "${var.identifier}-api_invoke-${random_string.this.id}"
+  role   = aws_iam_role.api_gw_role.name
+  policy = data.aws_iam_policy_document.api_gw_invoke_policy.json
 }
